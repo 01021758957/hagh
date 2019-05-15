979dde18250f78f4e9c6ee9232300eccce193c29
01021758957 <49417477+01021758957@users.noreply.github.com>
2019-05-15 08:35:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ if (message.content.startsWith(prefix + 'setavatar')) {
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'setT')) {
+if (message.content.startsWith(prefix + 'beso')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
